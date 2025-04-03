reorganization, something now seems to be wrong with the jit scaling

scaling_jax.py
@@ -6,10 +6,11 @@ Benchmark of the baseline implementation in JAX.
 import os
 
 import jax
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,2,3,5" 
+os.environ["CUDA_VISIBLE_DEVICES"] = "3,5,6,7" 
 
-from tinyfluids.jax_tinyfluids.jax_baseline import time_integration
-from tinyfluids.jax_tinyfluids.jax_baseline import DENSITY_INDEX, PRESSURE_INDEX
+from tinyfluids.jax_tinyfluids.sharding_helpers import pad, unpad
+from tinyfluids.jax_tinyfluids.time_integration import time_integration
+from tinyfluids.jax_tinyfluids.fluid import DENSITY_INDEX, PRESSURE_INDEX, VAR_AXIS, X_AXIS, Y_AXIS, Z_AXIS
 import timeit
 import jax.numpy as jnp
 
@@ -37,29 +38,33 @@ def setup_ics(num_cells, num_injection_cells=2):
 
     return primitive_state, grid_spacing
 
-def measure_execution_time(primitive_state, grid_spacing, t_final, gamma):
+def measure_execution_time(primitive_state, grid_spacing, t_final, gamma, shard_mapped):
     # execute once for compilation and warmup
 
-    final_state, num_iterations = time_integration(primitive_state, grid_spacing, t_final, gamma)
+    final_state, num_iterations = time_integration(primitive_state, grid_spacing, t_final, gamma, shard_mapped)
     final_state.block_until_ready()
+
+    if shard_mapped:
+        padding = ((0, 0), (1, 1), (1, 1), (0, 0))
+        final_state = unpad(final_state, padding, final_state.sharding)
 
     plot_results(final_state)
     
     # Create a function for timing
     def timed_execution():
-        final_state, _ = time_integration(primitive_state, grid_spacing, t_final, gamma)
+        final_state, _ = time_integration(primitive_state, grid_spacing, t_final, gamma, shard_mapped)
         final_state.block_until_ready()  # Ensure execution completes before timing stops
         
     # Measure execution time
     times = timeit.repeat(
         timed_execution,
-        repeat=3,  # More repeats for better statistics
-        number=1   # Number of calls per measurement
+        repeat = 3,  # More repeats for better statistics
+        number = 1   # Number of calls per measurement
     )
     
     # Calculate statistics
-    min_time = min(times) / 3  # Per single execution
-    mean_time = sum(times) / len(times) / 3
+    min_time = min(times)
+    mean_time = sum(times) / len(times)
 
     min_time_per_iteration = min_time / num_iterations
     
@@ -75,7 +80,7 @@ def plot_results(final_state):
     axs[1].set_title("Pressure")
     plt.savefig("figures/check_{:d}.png".format(num_cells))
 
-def make_scaling_plots(sharding = False, num_cells_list = [32, 64, 128, 256, 512, 1024]):
+def make_scaling_plots(sharding = False, shard_mapped = False, num_cells_list = [32, 64, 128, 256, 512, 1024]):
     
     t_final = 0.2
     gamma = 5/3
@@ -87,13 +92,17 @@ def make_scaling_plots(sharding = False, num_cells_list = [32, 64, 128, 256, 512
         primitive_state, grid_spacing = setup_ics(num_cells, num_cells // 16)
 
         if sharding:
-            sharding_mesh = jax.make_mesh((1, 2, 2, 1), ('variables', 'x', 'y', 'z'))
-            sharding = jax.NamedSharding(sharding_mesh, P('variables', 'x', 'y', 'z'))
+            sharding_mesh = jax.make_mesh((1, 2, 2, 1), (VAR_AXIS, X_AXIS, Y_AXIS, Z_AXIS))
+            sharding = jax.NamedSharding(sharding_mesh, P(VAR_AXIS, X_AXIS, Y_AXIS, Z_AXIS))
             primitive_state = jax.device_put(primitive_state, sharding)
 
             jax.debug.visualize_array_sharding(primitive_state[0, :, :, 0])
 
-        execution_time, execution_time_per_iteration = measure_execution_time(primitive_state, grid_spacing, t_final, gamma)
+            if shard_mapped:
+                padding = ((0, 0), (1, 1), (1, 1), (0, 0))
+                primitive_state = pad(primitive_state, padding, sharding)
+        
+        execution_time, execution_time_per_iteration = measure_execution_time(primitive_state, grid_spacing, t_final, gamma, shard_mapped)
         execution_times.append(execution_time)
         execution_times_per_iteration.append(execution_time_per_iteration)
 
@@ -124,21 +133,29 @@ def make_scaling_plots(sharding = False, num_cells_list = [32, 64, 128, 256, 512
     execution_times = jnp.array(execution_times, dtype=jnp.float32)
     execution_times_per_iteration = jnp.array(execution_times_per_iteration)
 
+    file_appendix = ""
+
+    if sharding and not shard_mapped:
+        file_appendix = "_sharding"
+    elif sharding and shard_mapped:
+        file_appendix = "_shard_mapped"
+
     # save the data for later analysis
     jnp.savez(
-        "results/scaling_data{}.npz".format("_sharding" if sharding else ""),
+        "results/scaling_data{}.npz".format(file_appendix),
         num_cells_list = num_cells_list,
         execution_times = execution_times,
         execution_times_per_iteration = execution_times_per_iteration
     )
 
     # save the figure
-    plt.savefig("figures/scaling{}.png".format("_sharding" if sharding else ""))
+    plt.savefig("figures/scaling{}.png".format(file_appendix))
 
 def plot_scaling_results():
     # load the data
     data_unsharded = jnp.load("results/scaling_data.npz")
     data_sharded = jnp.load("results/scaling_data_sharding.npz")
+    data_sharded_mapped = jnp.load("results/scaling_data_shard_mapped.npz")
 
     fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
@@ -147,6 +164,7 @@ def plot_scaling_results():
 
     axs[0].plot(data_unsharded["num_cells_list"], data_unsharded["execution_times"], 'o-', label = "unsharded")
     axs[0].plot(data_sharded["num_cells_list"], data_sharded["execution_times"], 'o-', label = "sharded (4 GPUs)")
+    axs[0].plot(data_sharded_mapped["num_cells_list"], data_sharded_mapped["execution_times"], 'o-', label = "sharded (4 GPUs, shard mapped)")
     # set x and y log scale
     axs[0].set_xscale("log", base = 2)
     axs[0].set_yscale("log", base = 2)
@@ -157,6 +175,7 @@ def plot_scaling_results():
     axs[0].set_ylabel("Execution time in s")
     axs[1].plot(data_unsharded["num_cells_list"], data_unsharded["execution_times_per_iteration"], 'o-', label = "unsharded")
     axs[1].plot(data_sharded["num_cells_list"], data_sharded["execution_times_per_iteration"], 'o-', label = "sharded (4 GPUs)")
+    axs[1].plot(data_sharded_mapped["num_cells_list"], data_sharded_mapped["execution_times_per_iteration"], 'o-', label = "sharded (4 GPUs, shard mapped)")
     # set x and y log scale
     axs[1].set_xscale("log", base = 2)
     axs[1].set_yscale("log", base = 2)
@@ -168,7 +187,11 @@ def plot_scaling_results():
     # plot the speedup
     common_length = min(len(data_unsharded["num_cells_list"]), len(data_sharded["num_cells_list"]))
     speedup = data_unsharded["execution_times"][0:common_length] / data_sharded["execution_times"][0:common_length]
+
+    speedup_mapped = data_unsharded["execution_times"][0:common_length] / data_sharded_mapped["execution_times"][0:common_length]
+
     axs[2].plot(data_unsharded["num_cells_list"][0:common_length], speedup, 'o-', label = "speedup")
+    axs[2].plot(data_unsharded["num_cells_list"][0:common_length], speedup_mapped, 'o-', label = "speedup (shard mapped)")
     # theoretical speedup is 4
     axs[2].plot(data_unsharded["num_cells_list"][0:common_length], jnp.ones_like(speedup) * 4, '--', label = "theoretical speedup")
     axs[2].legend()
@@ -181,7 +204,8 @@ def plot_scaling_results():
 
 
 
-# # make_scaling_plots(sharding = False, num_cells_list = [32, 64, 96, 128, 196, 256, 384, 512, 768])
-# make_scaling_plots(sharding = True, num_cells_list =  [32, 64, 96, 128, 196, 256, 384, 512, 768, 1024])
+# make_scaling_plots(sharding = False, shard_mapped = False, num_cells_list = [32, 64, 96, 128, 196, 256, 384, 512])
+make_scaling_plots(sharding = True, shard_mapped = False, num_cells_list  = [32, 64, 96, 128, 196, 256, 384, 512, 768])
+make_scaling_plots(sharding = True, shard_mapped = True, num_cells_list   = [32, 64, 96, 128, 196, 256, 384, 512, 768])
 
-# plot_scaling_results()
+plot_scaling_results()

test.py
@@ -1,10 +1,8 @@
 import jax
-from jax.experimental import mesh_utils, multihost_utils
+from jax.experimental import mesh_utils
 from jax.experimental.shard_map import shard_map
 from jax.sharding import Mesh, PartitionSpec as P, NamedSharding
 from jax import numpy as jnp
-import jaxdecomp
-from functools import partial
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "5,6,7,8" 
@@ -12,21 +10,16 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "5,6,7,8"
 # general
 from functools import partial
 
-# typechecking
-from beartype import beartype as typechecker
-from jaxtyping import Array, Float, jaxtyped
-from typing import Tuple, Union
-
 # split into 2x2x1 partitions
 pdims = (2, 2)
 global_shape = (16, 16, 16)
 
 # Create a mesh of devices based on pdims
 devices = mesh_utils.create_device_mesh(pdims)
-mesh = Mesh(devices, axis_names=(0, 1))
+mesh = Mesh(devices, axis_names = ("x", "y"))
 
 # Define the sharding spec
-sharding = NamedSharding(mesh, P(0, 1))
+sharding = NamedSharding(mesh, P("x", "y"))
 
 # random global array then put to device
 global_array = jax.random.uniform(
@@ -36,17 +29,20 @@ global_array = jax.random.uniform(
     maxval=1.0,
 )
 
-print(mesh.axis_names) 
+# print the true global min
+global_min = jnp.min(global_array)
+print("True global min:", global_min)
+
 global_array = jax.device_put(global_array, sharding)
 
 def min_test(array):
     # Compute the local minimum
     local_min = jnp.min(array)
+    print("Local min:", local_min)
     
     # Use collectives to compute the global minimum across all devices
-    global_min = jax.lax.pmin(local_min, axis_name=(0, 1))
-    
-    print(global_min)
+    global_min = jax.lax.pmin(local_min, axis_name = ("x", "y"))
+    print("Global min:", global_min)
     
     return array
 

tinyfluids/jax_tinyfluids/fluid.py
@@ -1,23 +1,7 @@
-"""
-This is a baseline first order Eulerian hydrodynamics solver
-with a HLL Riemann solver for the Euler equations written in JAX.
-
-Parallelization using shardmap and halo exchange.
-
-For a more feature-rich fluid code for astrophysics in JAX,
-check out https://github.com/leo1200/jf1uids, for other
-purposes https://github.com/tumaer/JAXFLUIDS.
-"""
-
 # general imports
 from functools import partial
 import jax.numpy as jnp
 import jax
-
-# sharding
-from jax.sharding import Mesh
-from jax.experimental.shard_map import shard_map
-from jax.sharding import Mesh, PartitionSpec as P, NamedSharding
 
 # typing
 from jaxtyping import Array, Float, Int, jaxtyped
@@ -36,73 +20,6 @@ VAR_AXIS = 0
 X_AXIS = 1
 Y_AXIS = 2
 Z_AXIS = 3
-
-def pad(input_array, padding, sharding):
-    sharded_pad = shard_map(
-        jnp.pad,
-        mesh = sharding.mesh,
-        in_specs = (sharding.spec, P()),
-        out_specs = sharding.spec,
-    )
-    return sharded_pad(input_array, padding)
-
-
-def unpad(input_array, padding, sharding):
-    def _slice_to_unpad(padded_shard, padding_spec):
-        return padded_shard[
-            tuple(slice(pad[0], -pad[1] if pad[1] > 0 else None) for pad in padding_spec)
-        ]
-
-    # Apply the slicing operation across shards using shard_map
-    sharded_unpad = shard_map(
-        _slice_to_unpad,
-        mesh=sharding.mesh,
-        in_specs=(sharding.spec, P()),
-        out_specs=sharding.spec
-    )
-
-    return sharded_unpad(input_array, padding)
-
-@partial(jax.jit, static_argnames=['axis', 'num_blocks_along_axis'])
-def send_right(shard, axis, num_blocks_along_axis):
-    left_perm = [(i, (i + 1) % num_blocks_along_axis) for i in range(num_blocks_along_axis)] #  -1 for non periodic
-    return jax.lax.ppermute(shard, perm=left_perm, axis_name = axis)
-
-@partial(jax.jit, static_argnames=['axis', 'num_blocks_along_axis'])
-def send_left(shard, axis, num_blocks_along_axis):
-    # Create permutation based on device IDs along the axis
-    left_perm = [((i + 1) % num_blocks_along_axis, i) for i in range(num_blocks_along_axis)] # -1 for non periodic
-    return jax.lax.ppermute(shard, perm=left_perm, axis_name = axis)
-
-@partial(jax.jit, static_argnames=['padding', 'axis'])
-def collect_right_along_axis(shard, padding, axis):
-    padding_axis = padding[axis]
-    return jax.lax.slice_in_dim(shard, -padding_axis[1] - padding_axis[0], -padding_axis[1], axis=axis)
-
-@partial(jax.jit, static_argnames=['padding', 'axis'])
-def collect_left_along_axis(shard, padding, axis):
-    padding_axis = padding[axis]
-    return jax.lax.slice_in_dim(shard, padding_axis[0], padding_axis[0] + padding_axis[1], axis=axis)
-
-@partial(jax.jit, static_argnames=['padding', 'num_blocks_along_axis', 'axis'])
-def _halo_exchange_along_axis(shard, padding, num_blocks_along_axis, axis):
-
-    left_halo = collect_left_along_axis(shard, padding, axis)
-    right_halo = collect_right_along_axis(shard, padding, axis)
-
-    right, left = send_left(left_halo, axis, num_blocks_along_axis), send_right(right_halo, axis, num_blocks_along_axis)
-
-    shard = shard.at[(slice(None, None),) * axis + (slice(0, padding[axis][0]),) + (slice(None, None),) * (shard.ndim - axis - 1)].set(left)
-    shard = shard.at[(slice(None, None),) * axis + (slice(-padding[axis][1], None),) + (slice(None, None),) * (shard.ndim - axis - 1)].set(right)
-
-    return shard
-
-@partial(jax.jit, static_argnames=['padding', 'split'])
-def _halo_exchange(shard, padding, split):
-    for _, axis in enumerate([X_AXIS, Y_AXIS, Z_AXIS]):
-        if split[axis] > 1:
-            shard = _halo_exchange_along_axis(shard, padding, split[axis], axis)
-    return shard
 
 # -------------------------------------------------------------
 # ================== ↓ Basic Fluid Equations ↓ ================
@@ -392,7 +309,7 @@ def _hll_solver(
 # -------------------------------------------------------------
 
 # -------------------------------------------------------------
-# ==================== ↓ Time Integration ↓ ===================
+# ==================== ↓ Single Time Step ↓ ===================
 # -------------------------------------------------------------
 
 
@@ -497,6 +414,7 @@ def _evolve_state_along_axis(
 
     return primitive_state
 
+
 @jaxtyped(typechecker=typechecker)
 @jax.jit
 def _evolve_state(
@@ -511,60 +429,7 @@ def _evolve_state(
 
     return primitive_state
 
-@jaxtyped(typechecker=typechecker)
-@jax.jit
-def time_integration(
-    primitive_state: STATE_TYPE,
-    grid_spacing: Union[float, Float[Array, ""]],
-    t_final: Union[float, Float[Array, ""]],
-    gamma: Union[float, Float[Array, ""]],
-) -> Tuple[STATE_TYPE, Union[float, Int[Array, ""]]]:
-    """
-    Evolve the state of the fluid in time.
-
-    Args:
-        primitive_state: The primitive state of the fluid,
-                         sharded according to the sharding_mesh.
-        sharding_mesh: The mesh used for sharding.
-        grid_spacing: The grid spacing.
-        dt: The time step.
-        gamma: The adiabatic index of the fluid.
-        num_steps: The number of time steps to evolve the state.
-
-    Returns:
-        The evolved state of the fluid.
-    """
-
-    def time_step_fn(state):
-        primitive_state, t, num_iterations = state
-
-        padding = ((0, 0), (1, 1), (1, 1), (0, 0))
-        split = (1, 2, 2, 1)
-        primitive_state = _halo_exchange(primitive_state, padding, split)
-
-        # the wave speed calculation is currently done twice, once
-        # for finding dt and once for the state update, which
-        # is not optimal
-        dt_local = _cfl_time_step(primitive_state, grid_spacing, gamma)
-
-        dt = jax.lax.pmin(dt_local, axis_name=(1.0, 2.0))
-
-        primitive_state = _evolve_state(primitive_state, grid_spacing, dt, gamma)
-
-        t += dt
-        num_iterations += 1
-
-        return primitive_state, t, num_iterations
-
-    def cond_fn(state):
-        _, t, _ = state
-        return t < t_final
-
-    initial_state = (primitive_state, 0.0, 0)
-    primitive_state, _, num_iterations = jax.lax.while_loop(cond_fn, time_step_fn, initial_state)
-
-    return primitive_state, num_iterations
 
 # -------------------------------------------------------------
-# ==================== ↑ Time Integration ↑ ===================
+# ==================== ↑ Single Time Step ↑ ===================
 # -------------------------------------------------------------

tinyfluids/jax_tinyfluids/sharding_helpers.py
@@ -1,19 +1,22 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "6,7,8,9" 
-
-VARIABLE_AXIS = 0
-X_AXIS = 1
-Y_AXIS = 2
-Z_AXIS = 3
-
-# general
+# general imports
 from functools import partial
-import jax
 import jax.numpy as jnp
+import jax
 
 # sharding
+from jax.sharding import Mesh
 from jax.experimental.shard_map import shard_map
-from jax.sharding import PartitionSpec as P
+from jax.sharding import Mesh, PartitionSpec as P, NamedSharding
+
+# typing
+from jaxtyping import Array, Float, Int, jaxtyped
+from beartype import beartype as typechecker
+from typing import Tuple, Union
+
+from tinyfluids.jax_tinyfluids.fluid import X_AXIS, Y_AXIS, Z_AXIS
+STATE_TYPE = Float[Array, "num_vars num_cells_x num_cells_y num_cells_z"]
+FIELD_TYPE = Float[Array, "num_cells_x num_cells_y num_cells_z"]
+
 
 def pad(input_array, padding, sharding):
     sharded_pad = shard_map(
@@ -43,24 +46,37 @@ def unpad(input_array, padding, sharding):
 
 @partial(jax.jit, static_argnames=['axis', 'num_blocks_along_axis'])
 def send_right(shard, axis, num_blocks_along_axis):
-    left_perm = [(i, (i + 1) % num_blocks_along_axis) for i in range(num_blocks_along_axis - 1)]
+    # use num_blocks_along_axis - 1 for non periodic situation
+    # but then the outer boundaries must be properly handled
+    left_perm = [(i, (i + 1) % num_blocks_along_axis) for i in range(num_blocks_along_axis)]
     return jax.lax.ppermute(shard, perm=left_perm, axis_name = axis)
 
 @partial(jax.jit, static_argnames=['axis', 'num_blocks_along_axis'])
 def send_left(shard, axis, num_blocks_along_axis):
-    # Create permutation based on device IDs along the axis
-    left_perm = [((i + 1) % num_blocks_along_axis, i) for i in range(num_blocks_along_axis - 1)]
+    # use num_blocks_along_axis - 1 for non periodic situation
+    # but then the outer boundaries must be properly handled
+    left_perm = [((i + 1) % num_blocks_along_axis, i) for i in range(num_blocks_along_axis)]
     return jax.lax.ppermute(shard, perm=left_perm, axis_name = axis)
 
 @partial(jax.jit, static_argnames=['padding', 'axis'])
 def collect_right_along_axis(shard, padding, axis):
     padding_axis = padding[axis]
-    return jax.lax.slice_in_dim(shard, -padding_axis[1] - padding_axis[0], -padding_axis[1], axis=axis)
+    return jax.lax.slice_in_dim(
+        shard, 
+        -padding_axis[1] - padding_axis[0],
+        -padding_axis[1],
+        axis = axis
+    )
 
 @partial(jax.jit, static_argnames=['padding', 'axis'])
 def collect_left_along_axis(shard, padding, axis):
     padding_axis = padding[axis]
-    return jax.lax.slice_in_dim(shard, padding_axis[0], padding_axis[0] + padding_axis[1], axis=axis)
+    return jax.lax.slice_in_dim(
+        shard,
+        padding_axis[0],
+        padding_axis[0] + padding_axis[1],
+        axis = axis
+    )
 
 @partial(jax.jit, static_argnames=['padding', 'num_blocks_along_axis', 'axis'])
 def _halo_exchange_along_axis(shard, padding, num_blocks_along_axis, axis):
@@ -68,10 +84,20 @@ def _halo_exchange_along_axis(shard, padding, num_blocks_along_axis, axis):
     left_halo = collect_left_along_axis(shard, padding, axis)
     right_halo = collect_right_along_axis(shard, padding, axis)
 
-    right, left = send_left(left_halo, axis, num_blocks_along_axis), send_right(right_halo, axis, num_blocks_along_axis)
+    right = send_left(left_halo, axis, num_blocks_along_axis)
+    left = send_right(right_halo, axis, num_blocks_along_axis)
 
-    shard = shard.at[(slice(None, None),) * axis + (slice(0, padding[axis][0]),) + (slice(None, None),) * (shard.ndim - axis - 1)].set(left)
-    shard = shard.at[(slice(None, None),) * axis + (slice(-padding[axis][1], None),) + (slice(None, None),) * (shard.ndim - axis - 1)].set(right)
+    shard = shard.at[
+        (slice(None, None),) * axis + 
+        (slice(0, padding[axis][0]),) + 
+        (slice(None, None),) * (shard.ndim - axis - 1)
+        ].set(left)
+    
+    shard = shard.at[
+        (slice(None, None),) * axis + 
+        (slice(-padding[axis][1], None),) + 
+        (slice(None, None),) * (shard.ndim - axis - 1)
+        ].set(right)
 
     return shard
 
@@ -81,34 +107,3 @@ def _halo_exchange(shard, padding, split):
         if split[axis] > 1:
             shard = _halo_exchange_along_axis(shard, padding, split[axis], axis)
     return shard
-
-
-x = jnp.ones((4, 4, 4, 4))
-split = (1, 2, 2, 1)
-sharding_mesh = jax.make_mesh(split, (VARIABLE_AXIS, X_AXIS, Y_AXIS, Z_AXIS))
-partition_spec = P(VARIABLE_AXIS, X_AXIS, Y_AXIS, Z_AXIS)
-sharding = jax.NamedSharding(sharding_mesh, partition_spec)
-x = jax.device_put(x, sharding)
-print(x[0, :, :, 0])
-
-padding = ((0, 0), (1, 1), (1, 1), (0, 0))
-x_pad = pad(x, padding, sharding)
-print(x_pad[0, :, :, 0])
-x_unpad = unpad(x_pad, padding, sharding)
-print(x_unpad[0, :, :, 0])
-
-sharded_halo_exchange = shard_map(
-    _halo_exchange,
-    mesh=sharding.mesh,
-    in_specs=(sharding.spec, None, None),
-    out_specs=sharding.spec
-)
-x_halo = sharded_halo_exchange(x_pad, padding, split)
-print(x_halo[0, :, :, 0])
-
-@partial(jax.jit, static_argnames=['axis'])
-def ttt(axis):
-    print(axis)
-    return(axis)
-
-ttt("hh")